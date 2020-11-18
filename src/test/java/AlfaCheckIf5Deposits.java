1890182c01c001f9c4bd8b046b94603b9add605d
KolobokTesto <dfilobok@omilia.com>
2020-11-18 16:00:28 +0200

    Initial commit

---
+++ src/test/java/AlfaCheckIf5Deposits.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class AlfaCheckIf5Deposits {
+
+    @Test
+    public void test(){
+
+        open("https://alfabank.ru");
+        $("[href=\"/make-money/\"]").click();
+        $("[data-test-id='button']").click();
+        $("[data-test-id='tabs-list-tabTitle-1']").click();
+        $("div[class='']").findAll("div[data-widget-name=\"CatalogCard\"]").shouldHaveSize(5);
+    }
+
+}
