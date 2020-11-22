3edfa3b40b07a36bc19f1155830f3a7c73fb9c98
KolobokTesto <dfilobok@omilia.com>
2020-11-22 20:21:03 +0200

    cosmetic change

--- src/test/java/AlfaCheckIf5Deposits.java
+++ src/test/java/AlfaCheckIf5Deposits.java
@@ -7,13 +7,14 @@ import static com.codeborne.selenide.Selenide.open;
 public class AlfaCheckIf5Deposits {
 
     @Test
-    public void test(){
-
+    public void test() {
         open("https://alfabank.ru");
         $("[href=\"/make-money/\"]").click();
         $("[data-test-id='button']").click();
         $("[data-test-id='tabs-list-tabTitle-1']").click();
-        $("div[class='']").findAll("div[data-widget-name=\"CatalogCard\"]").shouldHaveSize(5);
+        $("div[class='']")
+                .findAll("div[data-widget-name=\"CatalogCard\"]")
+                .shouldHaveSize(5);
     }
 
 }
